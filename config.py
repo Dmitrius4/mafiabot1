02b0fe811c5e219574666240568deddbe2ce7586
Dmitrius4 <dmitrius4@gmail.com>
2026-05-08 14:24:02 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 DB_PATH = os.getenv("DB_PATH", "mafia_bot.db").strip()
 
 if not BOT_TOKEN:
-    raise RuntimeError("Не задан BOT_TOKEN в переменных окружения.")
+    raise RuntimeError("Не задан BOT_TOKEN. Укажи переменную окружения BOT_TOKEN.")
